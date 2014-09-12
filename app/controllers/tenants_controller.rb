b297a62bf446b7f63130827d4f6cd10765a7d3b8
sean <shane.connolly@sumup.com>
2014-09-12 21:46:28 +0100

    got get working need to look at getting all tenants by household_id

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -2,14 +2,20 @@ class TenantsController < ApplicationController
   # GET /tenants
   # GET /tenants.json
   def index
-    @tenants = Tenant.all
+    puts 'showing index'
+    @tenants = Tenant.where(:house_account_id => 1)
 
     render json: @tenants
+
+    #respond_to do |format|
+    #  format.xml { render xml: @tenants }
+    #end
   end
 
   # GET /tenants/1
   # GET /tenants/1.json
   def show
+
     @tenant = Tenant.find(params[:id])
 
     render json: @tenant
@@ -18,7 +24,9 @@ class TenantsController < ApplicationController
   # POST /tenants
   # POST /tenants.json
   def create
-    @tenant = Tenant.new(params[:tenant])
+    puts "well boy"
+    puts ""
+    @tenant = Tenant.new(tenant_params)
 
     if @tenant.save
       render json: @tenant, status: :created, location: @tenant
@@ -46,5 +54,15 @@ class TenantsController < ApplicationController
     @tenant.destroy
 
     head :no_content
+  end
+
+
+  private
+
+  def tenant_params
+    # It's mandatory to specify the nested attributes that should be whitelisted.
+    # If you use `permit` with just the key that points to the nested attributes hash,
+    # it will return an empty hash.
+    params.require(:tenant).permit(:tenantId, :houseAccountId, :firstName, :lastName)
   end
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,4 @@
 class Tenant < ActiveRecord::Base
+
   belongs_to :house_account
 end
